92d706df34e6a3acaa4225047b7a7fc2d2aa76df
Hiroki Yagita <hiroki.yagita@gree.co.jp>
2015-03-03 00:17:52 +0900

    add test

--- spec/client_test.rb
+++ spec/client_test.rb
@@ -21,10 +21,56 @@ describe Hockey::Client do
   describe 'teams' do
 
     it 'when succeeded' do
-      # todo: mocking
-      # client = Hockey::Client.new ''
-      # teams = client.teams
-      # teams.must_be_instance_of Array
+      net_mock = Minitest::Mock.new
+      client = Hockey::Client.new 'token', network: net_mock
+
+      object =<<_EOS_
+{
+    "teams": [
+        {
+            "id": 23,
+            "name": "Bit Stadium GmbH Owners"
+        },
+        {
+            "id": 42,
+            "name": "External Testers"
+        }
+    ],
+    "status": "success",
+    "current_page": 1,
+    "per_page": 25,
+    "total_entries": 3,
+    "total_pages": 2
+}
+_EOS_
+      net_mock.expect(:get_object, JSON.parse(object), ['/api/2/teams'])
+      object =<<_EOS_
+{
+    "teams": [
+        {
+            "id": 50,
+            "name": "Bit Stadium GmbH Owners"
+        }
+    ],
+    "status": "success",
+    "current_page": 2,
+    "per_page": 25,
+    "total_entries": 3,
+    "total_pages": 2
+}
+_EOS_
+      net_mock.expect(:get_object, JSON.parse(object), ['/api/2/teams'])
+
+      teams = client.teams
+      teams.must_be_instance_of Array
+      teams.size.must_equal 3
+      teams.each do |obj|
+        obj.must_be_instance_of Hockey::Team
+      end
+      teams[0].id.must_equal 23
+      teams[1].id.must_equal 42
+      teams[2].id.must_equal 50
+      net_mock.verify
     end
 
   end
